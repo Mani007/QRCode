a19fc191ddff4b08fa477d1cc38b86fb25707a29
Mohnish <mohnish.a007@gmail.com>
2017-03-18 11:18:15 +0530

    improvement

--- qrflask/qrflask/__init__.py
+++ qrflask/qrflask/__init__.py
@@ -1,8 +1,3 @@
-"""
-This script runs the application using a development server.
-It contains the definition of routes and views for the application.
-"""
-
 from flask import Flask
 app = Flask(__name__)
 
@@ -10,10 +5,6 @@ app = Flask(__name__)
 wsgi_app = app.wsgi_app
 
 
-@app.route('/')
-def hello():
-    """Renders a sample page."""
-    return "Hello World!"
 
 if __name__ == '__main__':
     import os
