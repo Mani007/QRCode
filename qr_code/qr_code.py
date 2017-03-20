67ebaceb25039ebd5c881b69eb4f0c50b78d6ddc
Mohnish <mohnish.a007@gmail.com>
2017-03-20 23:06:19 +0530

    some additional feature
    to make QR code and read QR we can also add file location

--- qr_code/qr_code.py
+++ qr_code/qr_code.py
@@ -4,10 +4,16 @@ import qrtools
 img = qrcode.make('name: Niladri\n ID : 12345')
 
 img.save("filename.png")
+# we can also save file to a particular location 
+# img.save("C:/Users/all/Documents/GitHub/QRCode/qrflask/qrflask/tempimg/myQR.png")
 
 qr = qrtools.QR()
 
 qr.decode("filename.png")
+#from qrtools import QR
+#myCode = QR()
+#print myCode.decode_webcam()
+# The decode_webcam() method will open a zbar(driver written in C)  window that will let you scan for a code using your webcam
 
 print qr.data
 
